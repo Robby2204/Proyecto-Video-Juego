03a27ef3cb8d65ed34cd2db80c81641d716bbc83
MarcoCastilloP <marcopechc25@gmail.com>
2023-04-25 23:20:22 -0600

    avance movimiento
    Movimiento y animacion

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-import { Sitting } from "./playerStates.js";
+import { Sitting, Running, Jumping, Falling } from "/playerStates.js";
 
 export class Player {
     constructor(game){
@@ -11,15 +11,21 @@ export class Player {
         this.weight = 1;
         this.image = document.getElementById('player');
         this.frameX = 0;
-        this.frameY = 5; 
+        this.frameY = 0; 
+        this.maxFrame = 5;
+        this.fps = 20;
+        this.frameInterval = 1000/this.fps;
+        this.frameTimer = 0;
         this.speed = 0;
         this.maxSpeed = 10;
-        this.states = [new Sitting(this)];
+        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)]
+        ;
         this.currentState = this.states[0];
         this.currentState.enter();
     }   
     
-    update(input){
+    update(input, deltaTime){
+        this.currentState.handleInput(input)
         // horizontal movement
         this.x += this.speed;
         if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
@@ -28,10 +34,18 @@ export class Player {
         if (this.x < 0 ) this.x = 0;
         if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
         // vertical movement
-        if (input.includes('ArrowUp') && this.onGround()) this.vy -= 30;
         this.y += this.vy;
         if (!this.onGround()) this.vy += this.weight;
         else this.vy = 0;
+        // sprite animation
+        if(this.frameTimer > this.frameInterval){
+            this.frameTimer = 0;
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = 0;
+        } else{
+            this.frameTimer += deltaTime;
+        }
+
     }
     draw(context){
         context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, 
@@ -39,5 +53,9 @@ export class Player {
     }
     onGround(){
         return this.y >= this.game.height - this.height;
+    }
+    setState(state){
+        this.currentState = this.states[state];
+        this.currentState.enter();
     }
 }
