ef85d0145aba6f139171f0e26533eee6c3960291
MarcoCastilloP <marcopechc25@gmail.com>
2023-05-05 13:19:54 -0600

    Fondo, Movimiento

---
+++ background.js
@@ -0,0 +1,17 @@
+class Layer {
+    constructor(game, width, heigth, speedModifier, image){
+        this.game = game;
+        this.width = width;
+        this.heigth = heigth;
+        this.speedModifier = speedModifier;
+        this.image = image;
+        this.x = 0;
+        this.y = 0;
+    }
+    update(){
+
+    }
+    draw(){
+        
+    }
+}
