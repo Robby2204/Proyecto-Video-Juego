b1d4ad0956da891bfe23711b53341ef5a84555cf
MarcoCastilloP <marcopechc25@gmail.com>
2023-05-17 23:40:49 -0600

    mejora

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-import { Sitting, Running, Jumping, Falling, Rolling } from "/playerStates.js";
+import { Sitting, Running, Jumping, Falling, Rolling, Diving, Hit } from "/playerStates.js";
 
 export class Player {
     constructor(game){
@@ -18,7 +18,7 @@ export class Player {
         this.frameTimer = 0;
         this.speed = 0;
         this.maxSpeed = 10;
-        this.states = [new Sitting(this.game), new Running(this.game), new Jumping(this.game), new Falling(this.game), new Rolling(this.game)];
+        this.states = [new Sitting(this.game), new Running(this.game), new Jumping(this.game), new Falling(this.game), new Rolling(this.game), new Diving(this.game), new Hit(this.game)];
     }   
     
     update(input, deltaTime){
@@ -29,12 +29,15 @@ export class Player {
         if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
         else if (input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
         else this.speed = 0;
+        //horizontal boundaries
         if (this.x < 0 ) this.x = 0;
         if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
         // vertical movement
         this.y += this.vy;
         if (!this.onGround()) this.vy += this.weight;
         else this.vy = 0;
+        // vetical boundaries
+        if (this.y > this.game.height - this.height - this.game.groundMargin) this.y = this.game.height - this.height - this.game.groundMargin;
         // sprite animation
         if(this.frameTimer > this.frameInterval){
             this.frameTimer = 0;
@@ -67,10 +70,13 @@ export class Player {
                 enemy.y + enemy.height > this.y
             ){
                 enemy.markedForDeletion = true;
+                if (this.currentState === this.states[4] || this.currentState === this.states[5]){
+                    this.game.score++;
+                } else{
+
+                }
                 this.game.score++;
-            } else {
-                // no collission
-            }
+            } 
         });
     }
 }
